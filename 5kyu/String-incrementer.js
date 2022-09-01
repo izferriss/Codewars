2e891ee48ecfeef7e4de97309fa8f15c79136bfe
Ivan <94561520+izferriss@users.noreply.github.com>
2022-09-01 13:46:21 -0400

    added comments for legibility

--- 5kyu/String-incrementer.js
+++ 5kyu/String-incrementer.js
@@ -1,23 +1,3 @@
-// https://www.codewars.com/kata/54a91a4883a7de5d7800009c
-
-// Your job is to write a function which increments a string, to create a new string.
-
-// If the string already ends with a number, the number should be incremented by 1.
-// If the string does not end with a number. the number 1 should be appended to the new string.
-// Examples:
-
-// foo -> foo1
-
-// foobar23 -> foobar24
-
-// foo0042 -> foo0043
-
-// foo9 -> foo10
-
-// foo099 -> foo100
-
-// Attention: If the number has leading zeros the amount of digits should be considered.
-
 function incrementString (str)
 {
     var index = -1;
@@ -31,7 +11,7 @@ function incrementString (str)
         num = num.join("");
         num = parseInt(num) + 1;
     }
-
+    //Find starting index pos of number
     for(var i = 0; i < str.length; i++)
     {
         if(str[i] >= 0 && str[i] <= 9 && flag)
@@ -43,13 +23,16 @@ function incrementString (str)
     //If string contains numbers
     if(!flag)
     {
+        //Append non-numeric characters to output string
         for(var i = 0; i < index; i++)
         {
             output += str[i];
         }
+
+        //Pad number string with leading zeros if necessary and append to output
         output += String(num.toString()).padStart(str.length - index, '0');
     }
-    //If string does not contain numbers
+    //If string does not contain numbers, add 1
     else
     {
         output = str + "1";
