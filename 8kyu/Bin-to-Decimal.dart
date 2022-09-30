9cacb67041d11de88a93f3c3eef6d24c0ecb7d56
Ivan <94561520+izferriss@users.noreply.github.com>
2022-09-30 17:36:14 -0400

    Delete 8kyu directory

--- 8kyu/Bin-to-Decimal.dart
+++
@@ -1,8 +0,0 @@
-// https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
-
-// Complete the function which converts a binary number (given as a string) to a decimal number.
-
-int binToDec(bin)
-{
-    return int.parse(bin, radix: 2);
-}
