6d34bd880bdc8ab256cd4efafeac91d18bcfb42a
Ivan <94561520+izferriss@users.noreply.github.com>
2022-12-07 21:53:01 +0000

    Add files via Visual Studio Code

---
+++ JavaScript/6kyu/Find-the-odd-int.js
@@ -0,0 +1,44 @@
+// https://www.codewars.com/kata/54da5a58ea159efa38000836
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(A)
+{
+    if(A.length == 1)
+    {
+        return A[0];
+    }
+    else
+    {
+        let freq = new Map();
+        for(var i = 0; i < A.length; i++)
+        {
+            if(freq[A[i]])
+            {
+                freq[A[i]]++;
+            }
+            else
+            {
+                freq[A[i]] = 1;
+            }
+            
+        }
+
+        for (const [key, value] of Object.entries(freq))
+        {
+            if(value % 2 != 0)
+            {
+                 return parseInt(key);
+            }
+        }
+    }
+}
