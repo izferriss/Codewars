9053b925adca3bb7dac0a7b9571350a2c1d82abd
izferriss <ferrissi@gmail.com>
2022-12-15 13:34:40 -0500

    Add files via Visual Studio Code

---
+++ JavaScript/7kyu/Testing-1-2-3.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number=function(array)
+{
+    const results = [];
+    for(var i = 0; i < array.length; i++)
+    {
+        let toPush = (i + 1).toString() + ": " + array[i].toString();
+        results.push(toPush);
+    }
+
+    return results;
+}
