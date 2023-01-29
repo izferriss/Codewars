1f4dd76e0c6e67818d51c5649ec2d3b215a067b3
izferriss <ferrissi@gmail.com>
2023-01-29 17:51:51 -0500

    Add files via Visual Studio Code

---
+++ Python/7kyu/Switcheroo.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/57f759bb664021a30300007d
+
+# Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+# Example:
+
+# 'acb' --> 'bca'
+# 'aabacbaa' --> 'bbabcabb'
+
+def switcheroo(s):
+    return s.replace("a", "d").replace("b", "a").replace("d", "b")
