0639c60cbcbd142df3dbdbb3698c465ce89f9d60
izferriss <ferrissi@gmail.com>
2023-02-01 16:26:22 -0500

    Add files via Visual Studio Code

---
+++ Python/8kyu/UEFA-EURO-2016.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/57613fb1033d766171000d60
+
+# Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+# uefa_euro_2016(['Germany', 'Ukraine'],[2, 0]) # "At match Germany - Ukraine, Germany won!"
+# uefa_euro_2016(['Belgium', 'Italy'],[0, 2]) # "At match Belgium - Italy, Italy won!"
+# uefa_euro_2016(['Portugal', 'Iceland'],[1, 1]) # "At match Portugal - Iceland, teams played draw."
+
+def uefa_euro_2016(teams, scores):
+    return "At match " + teams[0] + " - " + teams[1] + ", " + ("teams played draw." if scores[0] == scores[1] else (teams[0] + " won!" if scores[0] > scores[1] else teams[1] + " won!"))
