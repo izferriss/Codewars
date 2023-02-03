369f1a227d6dc3e01ba3e1328a9782584a4814c5
izferriss <ferrissi@gmail.com>
2023-02-03 11:54:28 -0500

    Add files via Visual Studio Code

---
+++ Python/7kyu/Spacify.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/57f8ee485cae443c4d000127
+
+# Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+# spacify("hello world") # returns "h e l l o   w o r l d"
+
+def spacify(string):
+    result = ""
+    for c in string:
+        result += c + ' '
+    return result[:len(result) - 1]
