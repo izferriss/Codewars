6ca1d36a3ce64c93e51b8abc6497ee468a3e6292
izferriss <ferrissi@gmail.com>
2023-02-19 18:40:13 -0500

    Add files via Visual Studio Code

---
+++ SQL/7kyu/SQL-Basics-Truncating.sql
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/594a8fa5a2db9e5f290000c3
+
+-- Given the following table 'decimals':
+
+-- decimals table schema
+-- id
+-- number1
+-- number2
+-- Return a table with a single column towardzero where the values are the result of number1 + number2 truncated towards zero.
+
+SELECT TRUNC(number1 + number2) AS towardzero
+FROM decimals
