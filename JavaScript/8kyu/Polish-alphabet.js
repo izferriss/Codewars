4e100f8cd61248c06457dd924624bb1ca8512000
izferriss <ferrissi@gmail.com>
2025-02-19 13:30:52 -0500

    Add files via Visual Studio Code

---
+++ JavaScript/8kyu/Polish-alphabet.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/57ab2d6072292dbf7c000039/javascript
+
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+function correctPolishLetters(string) {
+  return string
+    .replaceAll("ą", "a")
+    .replaceAll("ć", "c")
+    .replaceAll("ę", "e")
+    .replaceAll("ł", "l")
+    .replaceAll("ń", "n")
+    .replaceAll("ó", "o")
+    .replaceAll("ś", "s")
+    .replaceAll("ź", "z")
+    .replaceAll("ż", "z");
+}
