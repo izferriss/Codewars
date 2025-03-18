231128c4e482080f1ac367e58dbcd1c7abbedf95
izferriss <ferrissi@gmail.com>
2025-03-18 08:55:12 -0400

    Add files via Visual Studio Code

---
+++ JavaScript/8kyu/Training-JS-18-Methods-of-String-object-concat-split-and-its-good-friend-join.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/57280481e8118511f7000ffa/javascript
+
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+// For example:
+
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+function splitAndMerge(string, separator) {
+  let ret = string.split(" ");
+  for (let i = 0; i < ret.length; i++) {
+    ret[i] = ret[i].split("").join(separator);
+  }
+  return ret.join(" ");
+}
