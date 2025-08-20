a8244d825321892e4285026015bca5bf088d6ea6
izferriss <ferrissi@gmail.com>
2025-08-20 08:27:47 -0400

    Add files via Visual Studio Code

---
+++ JavaScript/7kyu/Recursion-101.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/5b752a42b11814b09c00005d/train/javascript
+
+// In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
+
+// i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+// ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+// iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+// a and b will both be lower than 10E8.
+
+// More examples in tests cases. Good luck!
+
+function solve(a, b) {
+	while (true) {
+		if (a === 0 || b === 0) {
+			return [a, b];
+		}
+
+		if (a >= 2 * b) {
+			a = a - 2 * b;
+		} else if (b >= 2 * a) {
+			b = b - 2 * a;
+		} else {
+			return [a, b];
+		}
+	}
+}
