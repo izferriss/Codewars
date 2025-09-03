f6382007792416c03336703de1888a7ad035d905
izferriss <ferrissi@gmail.com>
2025-09-03 11:29:32 -0400

    Add files via Visual Studio Code

---
+++ JavaScript/6kyu/Stringing-Me-Along.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/55f4a44eb72a0fa91600001e/train/javascript
+
+// Implement a function that receives a string, and lets you extend it with repeated calls. When no argument is passed you should return a string consisting of space-separated words you've received earlier.
+
+// Note: There will always be at least 1 string; all inputs will be non-empty.
+
+// For example:
+
+// createMessage("Hello")("World!")("how")("are")("you?")() === "Hello World! how are you?"
+// Tip (helpful, but not necessary): Try using classes!
+
+// Good luck and happy coding!
+
+function createMessage(initialString) {
+	let words = [initialString];
+
+	function extendMessage(nextString) {
+		if (nextString === undefined) {
+			return words.join(" ");
+		} else {
+			words.push(nextString);
+			return extendMessage;
+		}
+	}
+	return extendMessage;
+}
