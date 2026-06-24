51a96c80388a554c8968822949276647fe159611
izferriss <ferrissi@gmail.com>
2026-06-24 12:21:27 -0400

    Add files via Visual Studio Code

---
+++ JavaScript/7kyu/Move-all-vowels.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/56bf3287b5106eb10f000899/train/javascript
+
+// Given a string as input, move all of its vowels to the end of the string, in the same order as they were before.
+
+// Vowels are (in this kata): a, e, i, o, u
+
+// Note: all provided input strings are lowercase.
+
+// Examples
+// "day"    ==>  "dya"
+// "apple"  ==>  "pplae"
+
+function moveVowel(input) {
+    return input.replace(/[aeiou]/g, '') + input.replace(/[^aeiou]/g, '');
+}
