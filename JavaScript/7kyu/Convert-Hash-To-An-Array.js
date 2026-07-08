0c0a16cdd186c90ec6ff00586c6aabc9ed15fe1c
izferriss <ferrissi@gmail.com>
2026-07-08 10:16:57 -0400

    Add files via Visual Studio Code

---
+++ JavaScript/7kyu/Convert-Hash-To-An-Array.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/59557b2a6e595316ab000046/javascript
+
+// Convert a hash into an array. Nothing more, Nothing less.
+
+// {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+// should be converted into
+
+// [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+
+// Good Luck!
+
+function convertHashToArray(hash) {
+	return Object.entries(hash);
+}
